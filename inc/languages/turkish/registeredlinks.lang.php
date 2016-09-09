c19a68df818c3d0fff7341f92d343e0b8a8a490a
Hüseyin Körbalta <hsynkrblta@gmail.com>
2016-09-09 17:39:39 +0300

    Türkçe Dil Dosyası
    Türkçe Dil Dosyası v1.0

---
+++ inc/languages/turkish/registeredlinks.lang.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Linkleri Gizleme Eklentisi v1.0 - Dil Dosyası v1.0
+https://huseyinkorbalta.com
+Copyright 2016  Huseyinkorbalta.Com
+*/
+$l['reglinks_text'] = '<font color="#c0392b"><strong>İndirme linklerini görebilmek için <a href="/member.php?action=register">üye ol</a>manız gerekiyor ya da giriş yapmanız gerekiyor.</strong></font>';
+?>

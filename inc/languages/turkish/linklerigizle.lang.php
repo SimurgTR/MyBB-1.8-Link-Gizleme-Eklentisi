0b660d9649c906fd595af0902045292f6a7ef1a7
Hüseyin Körbalta <hsynkrblta@gmail.com>
2016-09-10 20:14:19 +0300

    Update and rename registeredlinks.lang.php to linklerigizle.lang.php

---
+++ inc/languages/turkish/linklerigizle.lang.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Linkleri Gizleme Eklentisi v1.0 - Dil Dosyası v1.0
+https://huseyinkorbalta.com
+Copyright 2016  Huseyinkorbalta.Com
+*/
+$l['linklerigizle_mesaj'] = '<font color="#c0392b"><strong>Linkleri görebilmek için sitemize <a href="/member.php?action=register">üye ol</a>manız ya da giriş yapmanız gerekiyor.<i>Sitemize üyelikler ücretsizdir!</i></strong></font>';
+#Ekstra
+/*
+
+$2['linklerigizle_grup_ayari']=''
+*/
+?>
--- inc/languages/turkish/registeredlinks.lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-Linkleri Gizleme Eklentisi v1.0 - Dil Dosyası v1.0
-https://huseyinkorbalta.com
-Copyright 2016  Huseyinkorbalta.Com
-*/
-$l['reglinks_text'] = '<font color="#c0392b"><strong>İndirme linklerini görebilmek için <a href="/member.php?action=register">üye ol</a>manız ya da giriş yapmanız gerekiyor.</strong></font>';
-?>

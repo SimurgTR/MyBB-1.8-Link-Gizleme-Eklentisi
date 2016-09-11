8b9f29bb868df76c28406a10a28f76985bb05110
Hüseyin Körbalta <hsynkrblta@gmail.com>
2016-09-11 19:11:38 +0300

    Eklentinin admin paneli bölümü için Türkçe dil dosyası eklendi. v1.1

---
+++ inc/languages/turkish/admin/linklerigizle.lang.php
@@ -0,0 +1,14 @@
+<?php
+/*
+İndirme Linklerini Gizle Eklentisi Türkçe Dil Dosyası v1.1
+Tarih: 10.09.2016
+Copyright @ 2016 - Hüseyin Körbalta -> https://huseyinkorbalta.com
+*/
+$l['linklerigizle_isim'] = '<font color="#d35400"><strong><img src="/images/linklerigizle/icon.png" alt="icon" width="14" height="14"> İndirme Linklerini Gizleme Eklentisi</strong></font>';
+$l['linklerigizle_aciklama'] = 'Konularınızda paylaştığınız iç ve dış bağlantıları ziyaretçilerinize gizlemenizi sağlar.';
+$l['linklerigizle_ayarlar_isim'] = 'İndirme Linklerini Gizle Eklenti Ayarları';
+$l['linklerigizle_ayarlar_aciklama'] = 'Eklentinin ayarlarını bu kısımdan yapılandırabilirsiniz.';
+$l['linklerigizle_eklenti_acik'] = 'Eklenti Aktif Edilsin mi?';
+$l['linklerigizle_eklenti_acik_aciklama'] = 'Eklentiyi aktif etmek istiyorsanız bu ayarı evet seçiniz.';
+
+?>

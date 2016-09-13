590b98034e15fdff811ca1598f12b5cb3168baf0
Hüseyin Körbalta <hsynkrblta@gmail.com>
2016-09-13 16:12:09 +0300

    Xpserkan tarafından href linklerine nofollow etiketi eklendi.

--- inc/languages/turkish/linklerigizle.lang.php
+++ inc/languages/turkish/linklerigizle.lang.php
@@ -4,5 +4,5 @@
 Tarih: 10.09.2016
 Copyright @ 2016 - Hüseyin Körbalta -> https://huseyinkorbalta.com
 */
-$l['linklerigizle_mesaj'] = '<font color="#c0392b"><strong>İndirme linklerini görebilmek için sitemize <a href="/member.php?action=register" target="_blank" title="Üye Ol">üye ol</a> manız veya <a href="/member.php?action=login" target="_blank" title="Giriş Yap">giriş</a> yapmanız gerekiyor.<i>Sitemize üyelikler ücretsizdir!</i></strong></font>';
+$l['linklerigizle_mesaj'] = '<font color="#c0392b"><strong>İndirme linklerini görebilmek için sitemize <a rel="nofollow" href="/member.php?action=register" target="_blank" title="Üye Ol">üye ol</a> manız veya <a rel="nofollow" href="/member.php?action=login" target="_blank" title="Giriş Yap">giriş</a> yapmanız gerekiyor.<i>Sitemize üyelikler ücretsizdir!</i></strong></font>';
 ?>
